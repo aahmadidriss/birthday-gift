730c71d469a5dd8647df24374917d4999abf4f4b
aahmadidriss <39288886+aahmadidriss@users.noreply.github.com>
2025-09-26 21:41:24 +0700

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const slides = document.querySelectorAll("#slideshow img");
 let current = 0;
 
+// ganti foto tiap kali klik layar
 document.body.addEventListener("click", () => {
   slides[current].classList.remove("active");
   current = (current + 1) % slides.length;
   slides[current].classList.add("active");
 });
+
+// pastikan musik play setelah interaksi (aturan browser)
+document.body.addEventListener("click", () => {
+  const music = document.getElementById("bg-music");
+  if (music.paused) {
+    music.play();
+  }
+}, { once: true });
